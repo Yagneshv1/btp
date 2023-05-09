d233800f64e6a2065bf2f5a36d711b9c71129bb1
Yagnesh <72545632+Yagneshv1@users.noreply.github.com>
2023-05-10 01:43:22 +0530

    Updated stemmer settings

--- Frontend_Login.py
+++ Frontend_Login.py
@@ -542,11 +542,11 @@ if __name__ == "__main__":
 
 
 # 	requests.post(f"https://my-deployment-3de21f.es.us-central1.gcp.cloud.es.io/test_image/_close")
-	response = requests.put(f"https://my-deployment-3de21f.es.us-central1.gcp.cloud.es.io/test2/_settings", headers= headers, data = new_settings)
-	if response.status_code == 200:
-		st.write("Index settings updated successfully")
-	else:
-		st.write(f"Error updating index settings: {response.text}")
+# 	response = requests.put(f"https://my-deployment-3de21f.es.us-central1.gcp.cloud.es.io/test2/_settings", headers= headers, data = new_settings)
+# 	if response.status_code == 200:
+# 		st.write("Index settings updated successfully")
+# 	else:
+# 		st.write(f"Error updating index settings: {response.text}")
 # 	requests.post(f"https://my-deployment-3de21f.es.us-central1.gcp.cloud.es.io/test_image/_open")
 	st.title("IIT Palakkad Search Portal")
 	st.sidebar.image("iit-palakkad-logo.png")
