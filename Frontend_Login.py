1309eed1a384fa90bad98e0b95548c9a71d643ab
Yagnesh <72545632+Yagneshv1@users.noreply.github.com>
2023-05-10 07:19:51 +0530

    Update Frontend_Login.py

--- Frontend_Login.py
+++ Frontend_Login.py
@@ -23,7 +23,7 @@ from github import Github
 # nltk.download("punkt")
 # nltk.download("stopwords")
 #nltk.download('omw-1.4')
-nltk.download('wordnet')
+#nltk.download('wordnet')
 lm = WordNetLemmatizer()
 
 # Github credentials
