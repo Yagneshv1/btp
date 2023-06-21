6d2095fb0464b9db48c0b1fea741f2181553569b
Yagnesh <72545632+Yagneshv1@users.noreply.github.com>
2023-06-21 23:52:57 +0530

    Frontend_interface.py

--- Frontend_Login.py
+++ Frontend_Login.py
@@ -90,29 +90,23 @@ def login_user(username,password):
 
 
 def levenshtein_distance(s1, s2):
-    # Initialize a matrix of size (len(s1) + 1) x (len(s2) + 1) with 0s
     matrix = [[0 for j in range(len(s2) + 1)] for i in range(len(s1) + 1)]
     
-    # Fill in the first row and column with the indices
     for i in range(len(s1) + 1):
         matrix[i][0] = i
     for j in range(len(s2) + 1):
         matrix[0][j] = j
-    
-    # Fill in the rest of the matrix with the Levenshtein distances
+
     for i in range(1, len(s1) + 1):
         for j in range(1, len(s2) + 1):
             if s1[i-1] == s2[j-1]:
                 matrix[i][j] = matrix[i-1][j-1]
             else:
                 matrix[i][j] = 1 + min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1])
-    
-    # Return the Levenshtein distance between the two strings
+
     return matrix[len(s1)][len(s2)]
     
 def levenshtein_strings(word, distance):
-    #st.write(word)
-    # Generate all strings within the given Levenshtein distance from the word
     result = set()
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     d = enchant.Dict("en_US")
