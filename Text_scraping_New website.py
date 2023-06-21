02c05385097074df13760f8fdd1a77eb3aabf068
Yagnesh <72545632+Yagneshv1@users.noreply.github.com>
2023-06-22 00:46:34 +0530

    Update Text_scraping_New website.py
    Added documentation

--- Text_scraping_New website.py
+++ Text_scraping_New website.py
@@ -1,3 +1,6 @@
+#Code to perform text scraping for new institute website.
+
+#Import the necessary libraries
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -5,20 +8,24 @@ import csv
 import os
 import json
 
-visited = set()		#Set maintaing the list of all the pages visited so far.
-ignored_start = set()
-pages = 0		#Keep count of number of pages that are scraped.
-ignored_extensions = set(['png', 'jpeg', 'pdf', 'doc', 'jpg'])
-value = 0
-failed_preprocess = 0
-num_links = 0
-no_threshold = 0
-out_domain = 0
-already_visit = 0
-images = 0
+visited = set()		#Set maintains the list of all the pages visited so far.
+ignored_start = set()	#The sites from which the scraping is not taken forward
+pages = 0		#Count of the number of pages that are scraped.
+ignored_extensions = set(['png', 'jpeg', 'pdf', 'doc', 'jpg'])	#Page types not considered in text scraping
+value = 0		#Count of pages visited that are in ignored extensions
+failed_preprocess = 0	# Number of pages failed in the pre-processing step
+num_links = 0		# Total number of page links
+no_threshold = 0	# Count of pages failed scraping due to lack of threshold
+out_domain = 0		#Count of pages visited out of domain during scraping
+already_visit = 0	#Count of pages visited more than once during scraping
+images = 0		#Count of the number of images.
 
 def pre_process(site):
+    '''
+    Function to check if the site is a valid one to proceed for scraping
+    '''
     global value
+    #Check if the website is not an ignored extension and doesn't start with ignored urls
     if site.split('.')[-1].lower() in ignored_extensions:
         value += 1
         return False
@@ -27,15 +34,20 @@ def pre_process(site):
         if(site.startswith(k)):
             return False
 
+    #Irrelevant pages for scraping text
     if site.__contains__("mailto") or site.__contains__("taxonomy") or site.__contains__("xml"): #Taxonomy pages and mailto pages have no content nor body so discard.
         return False
-	
+
+    #Contact pages are ignored for text scraping
     if site.endswith("contact"):
         return False
     
     return True
 
 def scrape(site, count=30):
+    '''
+    Text Scraping for a site and redirecting to the next.
+    '''
     global pages
     global num_links
     global no_threshold
@@ -46,15 +58,18 @@ def scrape(site, count=30):
 
     #print(pages)
     print("\n\n")
-    if(count == 0):		#Once we got the limit we simply return 
+
+    #If there is no threshold for the page, return
+    if(count == 0):
         no_threshold += 1
         return
 
-    visited.add(site)	#Add the current page to visited
+    visited.add(site)	#Add the current page to visited set
 
     if(site.startswith("http:")):	#For http site connection issues occur so replace to https
         site = site.replace("http:", "https:")	
 
+    #Check if the site passes the conditions to scrape.
     if not pre_process(site):
         failed_preprocess += 1
         return
@@ -64,9 +79,11 @@ def scrape(site, count=30):
         r = requests.get(site, timeout=10)	#Get the site request
     except:
         return
-    
+
+    #Extract the HTML content of the page
     soup = BeautifulSoup(r.content, 'lxml')	#Parse HTML of page
-    
+
+    #Try to find the page title from potential divisions or classes as per HTML page pattern. Else the page link is considered as title.
     title = ''
     
     try:
@@ -77,16 +94,17 @@ def scrape(site, count=30):
         except:
             title = site
 
-    if title.__contains__("/"):		#Avoid / in file name creation. So, just replace.
+    #Replace slashes with other character to avoid misinterpreation of / as path seperator in filename.
+    if title.__contains__("/"):
         title = title.replace("/", " ")
 
-    
-    #print(links)
+    #The text content in domain pages are enclosed in grid-x main-content class
     if site.__contains__("https://iitpkd.ac.in"):	#For pages in iitpkd we have main defined for the content except for the home page.
         if site not in ["https://iitpkd.ac.in", "https://iitpkd.ac.in/"]:
             soup = soup.find(class_="grid-x main-content")
-    links = soup.find_all("a", href = True)			#Find the hyperlinks.    
-    #Removing the header and footer
+    links = soup.find_all("a", href = True)			#Find the hyperlinks. 
+	
+    #Removing the header and footer sections. It could be a seperate tag or as a class.
     try:
         soup.header.decompose()
         soup.footer.decompose()
@@ -98,7 +116,7 @@ def scrape(site, count=30):
         print(element)
         if element:
             element.decompose()
-        #print(soup)
+        
         element = soup.find(class_ = "footer")
         if element:
             element.decompose()
@@ -107,19 +125,19 @@ def scrape(site, count=30):
     
     pages += 1
     print("Title of current extracted page is", title)
+	
     message = ""
-    if site.startswith("https://iitpkd.ac.in/people"):	#For faculty pages we need all the content in the page including the intro sections.
+    #For pages of people, the entire content is extracted. For all others, only content in certain tags is considered.
+    if site.startswith("https://iitpkd.ac.in/people"):
         message += soup.getText()
     else:
         content = soup.find_all(["p", "h1", "h2", "h3", "li", "ul", "br", "a"])	#All tags where we extract the content from.
         for k in content:						#Get the page content
             message += k.getText()
                         
-                    
-    # message = ""
-    # message += soup.getText().strip()
     message = message.replace('\n', ' ')	#Replacing to remove the unnecessary \n from text
-	#Maintains the list of all the links in a page
+
+    #Collect all the hyperlink references in a page
     references = []
     for i in links:
         try:
@@ -142,16 +160,17 @@ def scrape(site, count=30):
         num_links += 1
         
         if j.startswith(("/", "?")):	#For relative paths, 
-            if j == "/":	#If its just / its the same path. So, just ignore it by adding to visited set.
+            if j == "/":	#If its just /, its the same path. So, just ignore it by adding it to the visited set.
                 j = site + "/"
                 visited.add(j)
             elif j == "?page=0":
                 j = site + j
                 visited.add(j)
-
+		    
+	#Combine the URLs to handle relative and absolute paths directly
         j = requests.compat.urljoin(site, j)
 			
-        if j not in visited:			#Visit the pages which are not visited only.
+        if j not in visited:			#Visit the pages which are not visited only within the institute domain.
             if j.__contains__('iitpkd') and not j.__contains__(".com"):	#For pages within the iitpkd domain, we have iitpkd name in the link.
                 print('Redirecting to', j, 'from', site)
                 scrape(j, count - 1)
@@ -165,7 +184,7 @@ if __name__ == "__main__":
         urls = f.read().split()
 
     URL = urls[0]
-	
+    #Add all ignored urls to visited so that they are not considered during the scraping chain.
     for u in urls[1:]:
         visited.add(u)
         ignored_start.add(u)
@@ -175,6 +194,8 @@ if __name__ == "__main__":
     os.mkdir("scraped_text")
     os.chdir(os.getcwd() + "/scraped_text")
     scrape(URL)
+	
+    #Print the important statistics
     print("The total pages scraped are", pages)
     print("The total number of file type pages", value)
     print(num_links/pages)
